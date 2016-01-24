2aac647fafb903a4675854b4a8b3187ad62c4e37
Jesse Huang <skouriskaze@gmail.com>
2016-01-24 04:19:28 -0500

    Finished Alien
    :facepunch:

--- 3D Labyrinth/Assets/Resources/Scripts/BulletController.cs
+++ 3D Labyrinth/Assets/Resources/Scripts/BulletController.cs
@@ -3,23 +3,29 @@ using System.Collections;
 
 public class BulletController : MonoBehaviour {
 
+    private Vector3 velocity;
+
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        velocity = rb.velocity;
+        Debug.Log(velocity);
 	}
 
 
     void OnCollisionEnter(Collision oCollision)
     {
-        gameObject.SetActive(false);
+        
         if (oCollision.gameObject.tag.Contains("Player"))
         {
-            oCollision.gameObject.transform.Translate(oCollision.relativeVelocity / 2);
+            //oCollision.gameObject.transform.Translate(velocity);
+            oCollision.gameObject.transform.Translate(-1 * velocity / 2f, Space.World);
         }
+        gameObject.SetActive(false);
     }
 }
---
+++ 3D Labyrinth/Assets/Resources/Scripts/MenuScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MenuScript : MonoBehaviour 
+{
+	public Button startText;
+	public Button exitText;
+	
+	void Start ()
+		
+	{
+
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+
+		
+	}
+	
+	public void StartLevel (string level) //this function will be used on our Play button
+		
+	{
+		Application.LoadLevel (level); //this will load our first level from our build settings. "1" is the second scene in our game
+
+	}
+	
+	public void ExitGame ()
+		
+	{
+		Application.Quit(); //this will quit our game. Note this will only work after building the game
+		
+	}
+	
+}
