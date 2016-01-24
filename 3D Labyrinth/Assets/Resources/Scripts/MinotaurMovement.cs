56ba4fe4db461ac0a0d49302188a2b4c599934e4
jiangshen <jiangshen16@icloud.com>
2016-01-24 06:38:16 -0500

    Script editing

--- 3D Labyrinth/Assets/Resources/Scripts/MinotaurMovement.cs
+++ 3D Labyrinth/Assets/Resources/Scripts/MinotaurMovement.cs
@@ -9,7 +9,7 @@ public class MinotaurMovement : MonoBehaviour {
 
     //Base speeds for straight and turn
     private const float BASE_VERTICAL_SPEED = 5;
-    private const float BASE_HORIZONTAL_SPEED = 2;
+    private const float BASE_HORIZONTAL_SPEED = 5;
     private const float BASE_COOLDOWN = 5;
     private const float BASE_DASH_DECAY = 0.25f;
     private const float BASE_DASH_MULTIPLIER = 5f;
@@ -45,10 +45,10 @@ public class MinotaurMovement : MonoBehaviour {
         fHorizontal = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
         
         //Vector for vertical movement
-        Vector3 vMovement = new Vector3(0, 0.0f, (fVertical * m_fDash) * Time.deltaTime);
+        //Vector3 vMovement = new Vector3(0, 0.0f, (fVertical * m_fDash) * Time.deltaTime);
 
         //Uses movement vector to move forward and horizontal value to rotate
-        rb.velocity = transform.forward * BASE_VERTICAL_SPEED * fVertical;
+		rb.velocity = transform.forward * fVertical * BASE_VERTICAL_SPEED * m_fDash;
         transform.Rotate(0, BASE_HORIZONTAL_SPEED * fHorizontal, 0);
 
         //Decrement dash
---
+++ 3D Labyrinth/Assets/Resources/Scripts/WinConditions.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WinConditions : MonoBehaviour {
+
+	private int m_nKillCount;
+	public Text txtAnnouncement;
+	public int m_nGoal;
+
+	private string m_szMessage;
+	private float m_fNotifTimer;
+	private float m_fDeathTimer;
+
+	private const float DEATH_TIMER = 10;
+	// Use this for initialization
+	void Start () {
+		m_nKillCount = 0;
+		m_szMessage = "";
+		m_fNotifTimer = 0;
+		m_nGoal = 2;
+		m_fDeathTimer = DEATH_TIMER;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_fNotifTimer > 0) {
+			m_fNotifTimer -= Time.deltaTime;
+		}
+		else
+		{
+			m_szMessage = "";
+		}
+
+		if (m_fDeathTimer > 0)
+		{
+			m_fDeathTimer -= Time.deltaTime;
+		} else {
+			m_szMessage = "Time has run out!";
+		}
+
+		txtAnnouncement.text = m_szMessage;
+	}
+
+	void OnCollisionEnter(Collision oCollide)
+	{
+		if (oCollide.gameObject.tag.Contains("Player"))
+		{
+			m_nKillCount++;
+			m_szMessage = "You have defeated a player";
+			m_fNotifTimer = 2;
+		}
+
+		if (m_nKillCount >= m_nGoal)
+		{
+			m_szMessage = "Minotaur has won!";
+		}
+
+
+	}
+}
